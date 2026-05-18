ec2915cf55a644d25b1bcf0eb9bfba15fd28c8a4
ThanhAn <thanhan271126@gmail.com>
2026-05-18 23:19:49 +0700

    backup2

--- QuanLiHocTap/obj/Debug/net10.0-windows/QuanLiHocTap.AssemblyInfo.cs
+++ QuanLiHocTap/obj/Debug/net10.0-windows/QuanLiHocTap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLiHocTap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73423c98cec382f10b2c6a5f5ff4535bfd4f302e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804fb3babe9dd49156c13c5550e7752efd9fc3b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLiHocTap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLiHocTap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QuanLiHocTap/obj/Debug/net10.0-windows/Views/Windows/LoginWindow.g.i.cs
+++ QuanLiHocTap/obj/Debug/net10.0-windows/Views/Windows/LoginWindow.g.i.cs
@@ -63,13 +63,13 @@ namespace QuanLiHocTap.Views.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/QuanLiHocTap;component/views/windows/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/QuanLiHocTap;V1.0.0.0;component/views/windows/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Windows\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -79,7 +79,7 @@ namespace QuanLiHocTap.Views.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
